4ce0a10d23e42e7b99bae2bba43b38f373895eb4
MindedCarnival8 <isaac117-@hotmail.com>
2023-07-11 08:01:14 -0600

    feat: (angular.json) css style v1.0.0

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
+
+
+
 export class HomeComponent {
+  
+  isFlipped: boolean = false;
+  name: string = '';
+  phone: string = '';
+  email: string = '';
+  subject: string = '';
+  message: string = '';
+
+  toggleCard() {
+    this.isFlipped = !this.isFlipped;
+  }
+
+  sendMail() {
+    // Lógica para enviar el correo
+    console.log('Enviando correo...');
+  }
+  
+}
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    // Otros módulos importados
+    FormsModule
+  ],
+  // Otros metadatos del módulo
+})
+export class AppModule { 
+
 
 }
