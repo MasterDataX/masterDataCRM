6c8aefa26529268d9b7516590884d898e4951408
Luis Enrique Corona <luisenrique.corona@kuantik.mx>
2023-12-03 09:41:58 -0600

    FIX: changes database

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,13 @@
 export const environment = {
   firebase: {
-    projectId: 'angular-fire-50d12',
-    appId: '1:564042033987:web:c7b7604e527f8beb5d5df6',
-    storageBucket: 'angular-fire-50d12.appspot.com',
-    apiKey: 'AIzaSyCeAyfabPDJjjMYhr2B_lgHzlN_LJEW4Xw',
-    authDomain: 'angular-fire-50d12.firebaseapp.com',
-    messagingSenderId: '564042033987',
-    measurementId: 'G-SW7WSB6BZ3',
+    apiKey: "AIzaSyCqsbCoqsWmc6TG8MUkXshG2moPp3vgZuA",
+authDomain: "conekta-2c642.firebaseapp.com",
+projectId: "conekta-2c642",
+storageBucket: "conekta-2c642.appspot.com",
+messagingSenderId: "263765737105",
+appId: "1:263765737105:web:e8cf1af2cacd0eea770930",
+measurementId: "G-3DM4H09M11"
   },
 };
+
+
