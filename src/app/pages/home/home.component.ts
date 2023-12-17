9aa6368b8411091f7a3627b6bee5a12f161f0f1c
Luis Enrique Corona <luisenrique.corona@kuantik.mx>
2023-12-17 10:16:02 -0600

    fix2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -34,57 +34,129 @@ export class HomeComponent {
     private route: ActivatedRoute
   ) {}
 
-ngOnInit(){
-  this.createForm();
-  this.getData()
+  ngOnInit() {
+    this.getData();
+    this.createForm();
+   this.uuid= this.route.snapshot.queryParamMap.get('uuid')
+    
+  }
 
-}
-createForm(){
-  this.jobs=new FormGroup({
-    name: new FormControl(""),
-    phone: new FormControl(""),
-    email: new FormControl("", Validators.email),
-    subject:new FormControl("",Validators.minLength(10)),
-    message: new FormControl("")
+  addJob() {
+    this.newJob = true;
+  }
 
-})
-}
+  createForm() {
+    this.jobs = new FormGroup({
+      name: new FormControl(''),
+      phone: new FormControl(''),
+      email: new FormControl('', Validators.email),
+      subject: new FormControl(''),
+      message: new FormControl('')
+    });
+  }
+
   toggleCard() {
     this.isFlipped = !this.isFlipped;
   }
 
-  addJob(){
-    this.newJob=true;
-  }
-  closeAddJob(){
-    this.newJob=false
+  sendMail() {
+    // Lógica para enviar el correo
+    console.log('Enviando correo...');
   }
 
-  getData(){
-    this.database.getDoc('Registros1','00001').subscribe((result: any)=>{
+  getData() {
+    this.databaseFirebaseService.getAllDocs('reportes').subscribe((result:any)=>{
       console.log(result)
-      this.listJobs.push(result.subject)
-    },(error=>{
-      console.log(error)
-    }))
+      result.map((data:any)=>{
+        this.reportes.push(data)
+      })
+    })
   }
 
-  setData(){
+  setJob() {
+    this.uuid= this.route.snapshot.queryParamMap.get('uid')
     const data = {
       name: this.jobs.value.name,
       phone: this.jobs.value.phone,
       email: this.jobs.value.email,
       subject: this.jobs.value.subject,
-      message: this.jobs.value.message
+      message: this.jobs.value.message, 
+      employment: this.uuid
+    };
+  
+    this.databaseFirebaseService.createDoc(data, 'reportes')
+  .then((docRef: any) => {
+    if (docRef && docRef.id) {
+      const docId = docRef.id;
+      this.uploadImages(docId);
+      this.newJob = false;
+      alert('Registrado correctamente')
+    } else {
+      console.error('La referencia al documento es undefined o no tiene una propiedad "id".', docRef);
     }
-    console.log('Esta es data',data)
-
-    this.database.createDoc(data,'Registros1', '00001').then(result=>{
-      console.log(result)
-    }).catch(error=>{
-      console.log('Algo salió mal', error)
-    });
-     
+  })
+  .catch((error) => {
+    console.error('Error al crear el documento:', error);
+  });
   }
   
+
+  uploadImages(docId: string) {
+    if (this.images.length === 0) {
+      // No hay imágenes para cargar
+      console.log('No hay imágenes para cargar.');
+      return;
+    }
+  
+    const imagePromises: Promise<string>[] = [];
+  
+    // Iterar sobre las imágenes y cargar cada una
+    this.images.forEach((image: File) => {
+      const filePath = `/images/${docId}/${image.name}`;
+      const uploadTask = this.storage.upload(filePath, image);
+  
+      // Get notified when the download URL is available
+      const downloadURL$ = uploadTask.then(snapshot => snapshot.ref.getDownloadURL());
+  
+      // Add the promise to the array
+      imagePromises.push(downloadURL$);
+    });
+  
+    // When all images are uploaded, save the URLs in your database
+    Promise.all(imagePromises)
+      .then(imageURLs => {
+        console.log('URLs de imágenes cargadas:', imageURLs);
+  
+        // Do something with the URLs, e.g., save them in your database
+        const imageData = {
+          images: imageURLs
+        };
+  
+        this.databaseFirebaseService.updateDoc(imageData, 'reportes', docId);
+      })
+      .catch(error => {
+        console.error('Error al cargar imágenes:', error);
+      });
+  }
+  
+  
+
+  handleImageChange(event: any) {
+    this.images = Array.from(event.target.files);
+  }
+  activarElemento(reporte: any) {
+    this.elementoActivo = reporte;
+  }
+
+  mostrarDetalles(reporte: any) {
+    this.detallesVisible = true;
+    this.reporteSeleccionado = reporte;
+  }
+
+  ocultarDetalles() {
+    this.detallesVisible = false;
+    this.elementoActivo = null;
+    this.reporteSeleccionado = null;
+  }
+
 }
